cb8ab0533be0eed9f285456e4efc8e0202cd5f48
Liz Ing-Simmons <liz.ingsimmons@gmail.com>
2021-02-23 14:35:10 +0100

    Added back original extract method

--- chess/get_structures.py
+++ chess/get_structures.py
@@ -13,11 +13,11 @@ from scipy.ndimage import zoom
 import warnings
 from future.utils import string_types
 from .helpers import (
-    load_regions, load_sparse_matrix, sub_matrix_from_edges_dict, GenomicRegion
+    load_regions, load_sparse_matrix, sub_matrix_from_edges_dict
     )
 warnings.filterwarnings("ignore")
 logger = logging.getLogger(__name__)
-import pandas as pd
+
 
 def get_sparse_matrix(regions, sparse_matrix):
     '''Get all the sparse HiC matrix from the bed file'''
@@ -56,49 +56,28 @@ def clipped_zoom(img, zoom_factor, **kwargs):
     return out
 
 
-def get_info_feature_square(labels, submatrix, outfile, position, region, area, reg, bin_size):
-    feature_data_list = []
-    for feature in regionprops(labels):
-        if feature.area > area:
-            minr, minc, maxr, maxc = feature.bbox
-            # calculate genomic coordinates
-            row_coords = (region.start + (bin_size * minr), region.start + (bin_size * maxr))
-            col_coords = (region.start + (bin_size * minc), region.start + (bin_size * maxc))
-            chrom = region.chromosome
-            row_region = f"{chrom}:{row_coords[0]}-{row_coords[1]}"
-            col_region = f"{chrom}:{col_coords[0]}-{col_coords[1]}"
-            # sort genomic coordinates to help with removing duplicates later
-            if row_coords[0] < col_coords[0]:
-                row_region, col_region = row_region, col_region
-            else:
-                row_region, col_region = col_region, row_region
-
-            submat = submatrix[minr:maxr, minc:maxc]
-
-            flat_mat = ";".join(str(x) for x in submat.flatten())
-
-            output_data = {
-                "region_pair_id": reg,
-                "feature_idx": position,
-                "minc": minc,
-                "maxc": maxc,
-                "minr": minr,
-                "maxr": maxr,
-                "row_coords": row_region,
-                "column_coords": col_region,
-                "matrix_data": flat_mat
-            }
-            feature_data_list.append(output_data)
+def get_info_feature(labels, submatrix, outfile, position, area, reg):
+    for region in regionprops(labels):
+        minr, minc, maxr, maxc = region.bbox
+        if region.area > area:
+            bx = (minc, maxc, maxc, minc, minc)
+            by = (minr, minr, maxr, maxr, minr)
+            y_min, y_max = np.min(by), np.max(by)
+            x_min, x_max = np.min(bx), np.max(bx)
+            submat = submatrix[y_min:y_max, x_min:x_max]
+            flat_mat = list(submat.flatten())
+            flat_mat.insert(0, reg)
+            flat_mat.insert(1, position)
+            flat_mat.insert(2, x_min)
+            flat_mat.insert(3, x_max)
+            flat_mat.insert(4, y_min)
+            flat_mat.insert(5, y_max)
+            outfile.write(','.join(map(str, flat_mat)) + '\n')
             position += 1
-
-    df = pd.DataFrame(feature_data_list)
-    df = df[-df.duplicated(subset=['row_coords', 'column_coords'])]
-
-    df.to_csv(outfile, mode='a', index=False, header=not path.exists(outfile))
     return position
 
 
-def extract_structures_square(
+def extract_structures(
     reference_edges,
     reference_regions,
     query_edges,
@@ -108,14 +87,13 @@ def extract_structures_square(
     windowsize,
     sigma_spatial,
     size_medianfilter,
-    closing_square,
-    min_area=5
+    closing_square
 ):
     pos_query = 0
     pos_reference = 0
 
-    gained_features_file = path.join(output, 'gained_features.csv')
-    lost_features_file = path.join(output, 'lost_features.csv')
+    w1 = open(path.join(output, 'gained_features.tsv'), '+w')
+    w2 = open(path.join(output, 'lost_features.tsv'), '+w')
 
     for pair_ix, reference_region, query_region in pairs:
         reference, ref_rs = sub_matrix_from_edges_dict(
@@ -128,29 +106,17 @@ def extract_structures_square(
             query_regions,
             query_region,
             default_weight=0.)
-
-        # it could be helpful to make this a user-defined parameter
-        # maybe as a % of the input matrix area?
-        # would need to be a very low % though, even 1% of area is much higher than the current value
-        area = int((min_area * np.shape(query)[0]) / 100)
-
-        # it's not clear to me why matrix dimensions are used to decide how many bins are used for histograms for
-        # denoising / smoothing.
-        # Would default values also be fine?
+        area = int((5 * np.shape(query)[0]) / 100)
         size = np.shape(query)[0]
-
-        # calculate log2 query/reference
-        # does this only work for contact probabilities at the moment?
-        # i.e. Juicer input will give unexpected results with unequal seq depth
-        # I think this only works for query and ref regions that are the same size
-        # is there a check for this?
         or_matrix = np.log(np.divide(query, reference))
         where_are_NaNs = np.isnan(or_matrix)
         or_matrix[where_are_NaNs] = 0.
         or_matrix[or_matrix == -inf] = 0.
         or_matrix[or_matrix == inf] = 0.
         std = np.std(or_matrix)
-
+        # thres1, thres2 = mean + 0.5 * std, mean - 0.5 * std
+        # positive = np.where(or_matrix > thres1, or_matrix, 0.)
+        # negative = np.where(or_matrix < thres2, or_matrix, 0.)
         positive = np.where(or_matrix > (0.5 * std), or_matrix, 0.)
         negative = np.abs(np.where(or_matrix < -(0.5 * std), or_matrix, 0.))
 
@@ -177,33 +143,36 @@ def extract_structures_square(
 
         # binarise
         if np.all(filter_positive == 0.):
-            # I think the aim of this is to avoid attempting thresholding if all values are the same
-            # so should be `filter_positive`?
             threshold_pos = positive
         else:
             filter1 = filters.threshold_otsu(filter_positive, nbins=size)
             threshold_pos = filter_positive > filter1
 
         if np.all(filter_negative == 0.):
-            # should be `filter_negative`?
             threshold_neg = negative
         else:
             filter2 = filters.threshold_otsu(filter_negative, nbins=size)
             threshold_neg = filter_negative > filter2
 
+        # zoom and rotate 45 degrees
+        zm1 = clipped_zoom(threshold_pos, 0.7)
+        rot1 = ndi.rotate(zm1, 45, reshape=False)
+        zm2 = clipped_zoom(threshold_neg, zoom_factor=0.7)
+        rot2 = ndi.rotate(zm2, 45, reshape=False)
+
         # Close morphology
-        img1 = closing(threshold_pos, square(closing_square))
+        img1 = closing(rot1, square(closing_square))
         label_x1 = label(img1)
-        img2 = closing(threshold_neg, square(closing_square))
+        img2 = closing(rot2, square(closing_square))
         label_x2 = label(img2)
 
-        # get Hi-C bin size / resolution in bp
-        # needed for calculating genomic coordinates from pixel coordinates
-        # is there an easier / more robust way of doing this?
-        hic_bin_size = reference_regions[0].end - reference_regions[0].start + 1
+        zm1_reference = clipped_zoom(reference, 0.7)
+        rot_reference = ndi.rotate(zm1_reference, 45, reshape=False)
+        zm1_query = clipped_zoom(query, 0.7)
+        rot_query = ndi.rotate(zm1_query, 45, reshape=False)
 
         # get output (file with label and submatrices)
         pos_query = get_info_feature(
-            label_x1, query, gained_features_file, pos_query, query_region, area, pair_ix, hic_bin_size)
+            label_x1, rot_query, w1, pos_query, area, pair_ix)
         pos_reference = get_info_feature(
-            label_x2, reference, lost_features_file, pos_reference, reference_region, area, pair_ix, hic_bin_size)
+            label_x2, rot_reference, w2, pos_reference, area, pair_ix)
