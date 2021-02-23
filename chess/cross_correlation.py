e18d36b99bd764e6ba43ae7a4a45b87d2513da5d
Liz Ing-Simmons <liz.ingsimmons@gmail.com>
2021-02-23 17:24:57 +0100

    Modified cross correlate function to use new extracted features data format

--- chess/cross_correlation.py
+++ chess/cross_correlation.py
@@ -21,37 +21,35 @@ from kneed import KneeLocator
 
 def correlate2d(file, output_folder, pairs):
     logger = logging.getLogger('')
-    # load all submatrices and correlate them
-    all_arrays = defaultdict(list)
-    information_regions = defaultdict(list)
+    # load region pairs
     pairs_dict = defaultdict(str)
     for pair_ix, reference_region, query_region in pairs:
-        pairs_dict[int(pair_ix)] = str(reference_region)+'-'+str(query_region)
+        pairs_dict[pair_ix] = str(reference_region)+'-'+str(query_region)
 
-    with open(file, 'r') as r:
-        for line in r:
-            region, position, x_min, x_max, y_min, y_max = line.split(',')[:6]
-            pair_id = pairs_dict[int(region)]
-            line_float = [float(x) for x in line.split(',')[6:]]
-            height, width = int(y_max) - int(y_min), int(x_max) - int(x_min)
-            mat = np.asanyarray(line_float).reshape(int(height), int(width))
-            all_arrays[int(position)].append(mat)
-            information_regions[int(position)].append((pair_id, int(position)))
+    # load features
+    df = pd.read_csv(file).set_index('feature_idx')
+    df['height'] = df['maxc'] - df['minc']
+    df['width'] = df['maxr'] - df['minr']
+    all_arrays = {feature_id: np.asfarray(matrix_data.split(';')).reshape(height, width) for
+                  (feature_id, matrix_data, height, width) in
+                  zip(df.index, df['matrix_data'], df['height'], df['width'])}
+    information_regions = {feature_id: (pair_id, feature_id) for (feature_id, pair_id) in
+                           zip(df.index, df['region_pair_id'])}
 
     logger.info(
         '[MAIN]: All submatrices loaded, starting 2D cross-correlation')
-    tag = file.split('/')[-1].split('_')[0]
-    correlation_dataframe = pd.DataFrame(
-        index=range(len(all_arrays.keys())),
-        columns=range(len(all_arrays.keys())))
 
-    for a, b in tqdm(itertools.combinations(range(len(all_arrays.keys())), 2)):
-        c11 = c2d(all_arrays[a][0], all_arrays[b][0], mode='same')
-        c112 = c2d(all_arrays[b][0], all_arrays[a][0], mode='same')
-        transp1 = c2d(
-            np.fliplr(all_arrays[a][0]), all_arrays[b][0], mode='same')
-        transp2 = c2d(
-            np.fliplr(all_arrays[b][0]), all_arrays[a][0], mode='same')
+    tag = file.split('/')[-1].split('_')[0]
+    # calculate correlations
+    correlation_dataframe = pd.DataFrame(
+        index=df.index,
+        columns=df.index
+    )
+    for a, b in tqdm(itertools.combinations(df.index, 2)):
+        c11 = c2d(all_arrays[a], all_arrays[b], mode='same')
+        c112 = c2d(all_arrays[b], all_arrays[a], mode='same')
+        transp1 = c2d(np.fliplr(all_arrays[a]), all_arrays[b], mode='same')
+        transp2 = c2d(np.fliplr(all_arrays[b]), all_arrays[a], mode='same')
         best = max(c11.max(), c112.max(), transp1.max(), transp2.max())
         correlation_dataframe.loc[a, b] = best
         correlation_dataframe.loc[b, a] = best
@@ -65,16 +63,16 @@ def correlate2d(file, output_folder, pairs):
     correlation_dataframe.to_csv(
         path.join(output_folder, 'correlation_dataframe_%s.csv' % (tag)))
 
-    Sum_of_squared_distances = []
-    K = range(1, 15)
+    sum_of_squared_distances = []
+    K = range(1, min(scaled.shape[0], 15))
     for k in K:
         km = KMeans(n_clusters=k)
         km = km.fit(scaled)
-        Sum_of_squared_distances.append(km.inertia_)
+        sum_of_squared_distances.append(km.inertia_)
 
     kn = KneeLocator(
-        range(1, len(Sum_of_squared_distances)+1),
-        Sum_of_squared_distances,
+        range(1, len(sum_of_squared_distances)+1),
+        sum_of_squared_distances,
         curve='convex',
         direction='decreasing')
     optimal_number_clusters = kn.knee
@@ -83,8 +81,8 @@ def correlate2d(file, output_folder, pairs):
     plt.xlabel('number of clusters k')
     plt.ylabel('Sum of squared distances')
     plt.plot(
-        range(1, len(Sum_of_squared_distances)+1),
-        Sum_of_squared_distances,
+        range(1, len(sum_of_squared_distances)+1),
+        sum_of_squared_distances,
         'bx-')
     plt.vlines(kn.knee, plt.ylim()[0], plt.ylim()[1], linestyles='dashed')
     plt.savefig(path.join(output_folder, 'Elbow_index_%s.pdf' % tag))
